e7b26fb72cd8ffa8578287412de7513e2eb50aa0
ANDyouNo <younofl312@gmail.com>
2024-06-22 11:12:13 +0300

    11-5

--- src/pages/Error/Error.tsx
+++ src/pages/Error/Error.tsx
@@ -1,12 +1,8 @@
-import { Link } from "react-router-dom";
+
 
 export function Error() {
   return (
     <>
-      <div>
-        <Link to="/">Menu</Link>
-        <Link to="/cart">Cart</Link>
-      </div>
       <h1>404</h1>
     </>
   );
--- src/pages/Menu/Menu.tsx
+++ src/pages/Menu/Menu.tsx
@@ -1,12 +1,8 @@
-import { Link } from "react-router-dom";
 
 export function Menu() {
   return (
     <>
-      <div>
-        <Link to="/">Menu</Link>
-        <Link to="/cart">Cart</Link>
-      </div>
+      
       Menu
     </>
   );
