69ec42c003e4e60c3ca24a211e0df1f2f5780522
ANDyouNo <younofl312@gmail.com>
2024-06-23 12:15:16 +0300

    11-8

--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -1,15 +1,10 @@
-import { Link, Outlet, useLocation } from "react-router-dom";
+import { Link, Outlet, useLocation, NavLink } from "react-router-dom";
 import Button from "../components/Button/Button";
 import { useEffect } from "react";
 import classNames from "classnames";
 
 export function Layout() {
-	const location = useLocation()
 
-	useEffect(() => {
-		
-		console.log(location)
-	}, [location]);
   return (
     <div className="flex h-dvh w-full bg-white">
       <div className="p-7 border border-transparent border-r-separator flex flex-col ">
@@ -23,30 +18,34 @@ export function Layout() {
           <div className="mt-2 text-primary-default">younofl312@gmail.com</div>
         </div>
         <div className="flex flex-col gap-8">
-          <Link
-            className={classNames(
-              "ease-in-out duration-300 flex items-center gap-3 hover:text-primary-default",
-              {
-                "text-primary-dark": location.pathname == "/",
-              }
-            )}
+          <NavLink
+            className={({ isActive }) =>
+              classNames(
+                "ease-in-out duration-300 flex items-center gap-3 hover:text-primary-default",
+                {
+                  "text-primary-dark": isActive,
+                }
+              )
+            }
             to="/"
           >
             <img src="./menuBook.svg" alt="Menu-icon" />
             Menu
-          </Link>
-          <Link
-            className={classNames(
-              "ease-in-out duration-300 flex items-center gap-3 hover:text-primary-default",
-              {
-                "text-primary-dark": location.pathname == "/cart",
-              }
-            )}
+          </NavLink>
+          <NavLink
+            className={({ isActive }) =>
+              classNames(
+                "ease-in-out duration-300 flex items-center gap-3 hover:text-primary-default",
+                {
+                  "text-primary-dark": isActive,
+                }
+              )
+            }
             to="/cart"
           >
             <img src="./shoppingCart.svg" alt="Cart-icon" />
             Cart
-          </Link>
+          </NavLink>
         </div>
 
         <Button className="mt-auto">
