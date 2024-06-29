c9ce284d0f4cfed47450d3ebf2c686cc6a6b08b4
ANDouNo <younofl312@gmail.com>
2024-06-29 12:11:09 +0300

    11-10

---
+++ src/components/ProdutCard/ProductCard.tsx
@@ -0,0 +1,34 @@
+import { ProductCardProps } from "./ProductCard.props";
+import { Link } from "react-router-dom";
+const ProductCard = (props: ProductCardProps) => {
+  return (
+    <Link to={'./'}>
+      <div className="bg-white rounded-2xl drop-shadow-md w-80">
+        <div
+          className="relative rounded-2xl  min-h-40"
+          style={{ backgroundImage: `url('./product-demo.jpeg')` }}
+        >
+          <div className="absolute top-4 left-4 bg-white py-1 px-3 rounded-full text-lg">
+            {props.price}&nbsp;
+            <span className="text-primary-default">₽</span>
+          </div>
+          <button className="absolute bottom-[-20px] right-5 bg-primary-default p-3 rounded-full ease-in-out duration-300 hover:bg-primary-dark drop-shadow-sm">
+            <img src="./shoppingCartButton.svg" alt="add to cart" />
+          </button>
+          <div className="absolute drop-shadow-sm bottom-[-14px] left-4 bg-white py-1 px-3 rounded-full text-sm flex gap-1 justify-center items-center">
+            {props.rathing}
+            <img src="./star-icon.svg" alt="" />
+          </div>
+        </div>
+        <div className="p-3">
+          <div className="text-lg font-bold mt-2">{props.title}</div>
+          <div className="text-sm font-light text-secondary">
+            {props.description}
+          </div>
+        </div>
+      </div>
+    </Link>
+  );
+};
+
+export default ProductCard;
--- src/pages/Menu/Menu.tsx
+++ src/pages/Menu/Menu.tsx
@@ -1,5 +1,6 @@
 import Heading from "../../components/Heading/Headind";
 import Search from "../../components/Search/Search";
+import ProductCard from "../../components/ProdutCard/ProductCard";
 export function Menu() {
   return (
     <>
@@ -7,6 +8,16 @@ export function Menu() {
         <Heading>Menu</Heading>
         <Search placeholder="Введите блюдо или состав"></Search>
       </div>
+			<div>
+				<ProductCard
+				id={1}
+				title='Наслаждение'
+				description='Салями, Рукола, Помидоры, Оливки'
+				image={'./sdfsdf'}
+				price={300}
+				rathing={4.5}
+				/>
+			</div>
     </>
   );
 }
